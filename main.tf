3f6fa5d3d4b23b390fa27975d7eb313a7964f09b
esantanamat <esantanamat@gmail.com>
2025-03-25 13:45:28 -0400

    finalize ci/cd project

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "allow_ssh" {
       from_port   = ingress.value
       to_port     = ingress.value
       protocol    = "tcp"
-      cidr_blocks = ingress.value == 22 ? [var.my_ip] : ["0.0.0.0/0"]
+      cidr_blocks = ingress.value == 22 ? ["0.0.0.0/0"] : ["0.0.0.0/0"]
     }
   }
 
@@ -21,18 +21,69 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
 
-  tags = {
-    Name = "allow_ssh_http"
+  ingress {
+    from_port = 9000
+    to_port = 9000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port = 8085
+    to_port = 8085
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_instance" "my_ec2" {
+resource "aws_security_group" "jenkins_sg" {
+  name = "jenkins_sg"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Only allow your IP
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2_instances" {
   for_each = var.ec2_instances
   ami = each.value.ami
   instance_type = each.value.type
   key_name = aws_key_pair.my_key.key_name
   security_groups = [aws_security_group.allow_ssh.name]
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_instance" "jenkins_ec2" {
+  for_each = var.jenkins_instance
+  ami = each.value.ami
+  instance_type = each.value.type
+  key_name = aws_key_pair.my_key.key_name
+  security_groups = [aws_security_group.jenkins_sg.name]
   tags = {
     Name = each.key
   }
@@ -53,3 +104,12 @@ output "private_key" {
   sensitive = true
 }
 
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.my_key.private_key_pem
+  filename = "my-private-key.pem"
+}
+
+resource "local_file" "public_key" {
+  content  = tls_private_key.my_key.public_key_openssh
+  filename = "my-key.pub"
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,22 @@ variable "region" {
   type    = string
   default = "us-east-1"
 }
+
+
+variable "ec2_instances" {
+  default = {
+    "SonarQube"  = { type = "t3.medium", ami = "ami-08c40ec9ead489470" }
+    "Docker"  = { type = "t2.micro", ami = "ami-08c40ec9ead489470" }
+  }
+}
+
+variable "jenkins_instance" {
+  default = {
+    "jenkins" = { type = "t2.micro", ami = "ami-08c40ec9ead489470" }
+  }
+}
+
+variable "my_ip" {
+  type    = string
+}
+
